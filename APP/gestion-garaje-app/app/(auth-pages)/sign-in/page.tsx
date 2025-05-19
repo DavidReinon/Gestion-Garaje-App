ebc36f892ec78f35a0b126e7531d6df72c2db617
DavidReinon <dreinon@pavabits.com>
2025-05-19 12:02:33 +0200

    feat: add background image in sign in screen

--- APP/gestion-garaje-app/app/(auth-pages)/sign-in/page.tsx
+++ APP/gestion-garaje-app/app/(auth-pages)/sign-in/page.tsx
@@ -8,27 +8,45 @@ export default async function Login(props: { searchParams: Promise<Message> }) {
     const searchParams = await props.searchParams;
 
     return (
-        <form className="flex flex-col gap-4 p-6 max-w-md w-full bg-neutral-50 rounded-lg shadow-md">
-            <h1 className="text-2xl font-bold">Iniciar Sesión</h1>
-            <FormMessage message={searchParams} />
-            <Label htmlFor="email">Correo Electrónico</Label>
-            <Input
-                type="email"
-                name="email"
-                placeholder="you@example.com"
-                required
+        <>
+            <div
+                style={{
+                    backgroundImage: `url('/images/fondo-garaje-pantalla-sign-in.webp')`,
+                    backgroundSize: "cover",
+                    backgroundPosition: "center",
+                    width: "100vw",
+                    height: "100vh",
+                    position: "fixed",
+                    top: 0,
+                    left: 0,
+                    zIndex: -1,
+                }}
             />
-            <Label htmlFor="password">Contraseña</Label>
-            <Input
-                type="password"
-                name="password"
-                placeholder="Tu contraseña"
-                required
-            />
-            <Button className="mt-3 " type="submit" formAction={signInAction}>
-                Iniciar Sesión
-            </Button>
-            {/* <p className="text-sm text-gray-500">
+            <form className="flex flex-col gap-4 p-6 max-w-xs md:max-w-md w-full bg-neutral-50 rounded-lg shadow-md">
+                <h1 className="text-2xl font-bold">Iniciar Sesión</h1>
+                <FormMessage message={searchParams} />
+                <Label htmlFor="email">Correo Electrónico</Label>
+                <Input
+                    type="email"
+                    name="email"
+                    placeholder="you@example.com"
+                    required
+                />
+                <Label htmlFor="password">Contraseña</Label>
+                <Input
+                    type="password"
+                    name="password"
+                    placeholder="Tu contraseña"
+                    required
+                />
+                <Button
+                    className="mt-3 "
+                    type="submit"
+                    formAction={signInAction}
+                >
+                    Iniciar Sesión
+                </Button>
+                {/* <p className="text-sm text-gray-500">
                     ¿Olvidaste tu contraseña?{" "}
                     <Link
                         href="/forgot-password"
@@ -37,6 +55,7 @@ export default async function Login(props: { searchParams: Promise<Message> }) {
                         Recuperar
                     </Link>
                 </p> */}
-        </form>
+            </form>
+        </>
     );
 }
